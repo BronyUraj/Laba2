5067e8900475adbebc61aaa00e457c35704c5ea8
BronyUraj <76591985+BronyUraj@users.noreply.github.com>
2021-02-17 21:27:15 +0300

    Fix task1

--- Laba2/Task1.cpp
+++ Laba2/Task1.cpp
@@ -5,6 +5,6 @@ bool isInArea(double x, double y) {
 	if (x >= 0 && y >= 0)
 		return (pow(x, 2) + pow(y, 2) <= 1);
 	else if (x <= 0 && y <= 0)
-		return (x <= 1 && y <= 1);
+		return (x >= -1 && y >= -1);
 	return 0;
 }
